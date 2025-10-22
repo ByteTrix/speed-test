7c4bb7b337eae62e0613258f05672513f70192e2
Kavin T <91929982+itskavin@users.noreply.github.com>
2025-10-23 00:30:39 +0530

    vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google"; // Import Inter
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 // Configure Inter font
 const inter = Inter({
@@ -30,6 +31,7 @@ export default function RootLayout({
         className={`${inter.className} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
